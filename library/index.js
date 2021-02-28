776641eb16d2b73e9f60d959ba2fde23e0046a3d
cdrbbt <egor.bykadorov@gmail.com>
2021-02-28 18:08:41 +0200

    8.3 all authors

--- library/index.js
+++ library/index.js
@@ -88,6 +88,7 @@ const typeDefs = gql`
     bookCount: Int!
     authorCount: Int!
     allBooks: [Book!]!
+    allAuthors: [Author!]!
   }
 
   type Book {
@@ -97,13 +98,25 @@ const typeDefs = gql`
     id: ID!
     genres: [String!]!
   }
+
+  type Author {
+    name: String!
+    id: ID!
+    born: Int
+    bookCount: Int!
+  }
 `
 
 const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books
+    allBooks: () => books,
+    allAuthors: () => authors
+  },
+
+  Author: {
+    bookCount: (root) => books.filter(b => b.author === root.name).length
   }
 }
 
